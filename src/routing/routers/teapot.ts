2c0f30405baa0da5d648c9669736d11dfdcbb654
TNTzx <xanderandrewritua@gmail.com>
2023-01-06 16:48:34 +0800

    added playground run configuration

--- src/routing/routers/teapot.ts
+++ src/routing/routers/teapot.ts
@@ -1,16 +1,15 @@
-import Express from "express"
-import { ExtendedReq, ExtendedRes } from "../lib/middlewares/override";
+import { RouterInfo } from "../routerClass"
+import { ExtendedReq, ExtendedRes } from "../../lib/middlewares/override";
 
 
 
-const router = Express.Router();
+const routerInfo = new RouterInfo("/teapot", ["/test", "/cock"]);
 
-router.all(
+routerInfo.router.all(
     "/",
     (req: ExtendedReq, res: ExtendedRes) => {
         return res.message(418, {message: "I'm a teapot! However, I cannot brew you coffee."});
     }
 );
 
-export default router;
-export const aliases = ["/test", "/cock"];
+export default routerInfo;
