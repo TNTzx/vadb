3d8b672332f75b3a4dd21a67688cfd77697e9f57
Uncubically <>
2023-01-11 17:56:20 +0800

    tag

---
+++ src/structs/tag.ts
@@ -0,0 +1,9 @@
+export default class Tag {
+    tag: string
+    description: string
+
+    constructor(tag: string, description: string) {
+        this.tag = tag
+        this.description = description
+    }
+}
