c3eedf403e8dc406fd78290d4f674e789d3da2b2
Uncubically <>
2023-01-11 17:56:35 +0800

    starting artist new model

--- src/structs/artist.ts
+++ src/structs/artist.ts
@@ -1,57 +1,92 @@
-/**
- * Contains information about the artist's availability.
- */
-export class Availability {
-    tag: string;
-    description: string;
+/** Represents an allowed song that can be used for Project Arrhythmia. */
+export class Song {
+    title: string;
+    aliases: string[];
+    audioPath: string;
+}
 
-    constructor(tag: string, description: string) {
-        this.tag = tag;
-        this.description = description;
+
+
+/** Contains additional metadata for the artist. */
+export class ArtistMetadata {
+    aliases: string[];
+    description?: string;
+    notes?: string;
+
+    genre?: string;
+    songs: Song[];
+    socials: string[];
+
+    constructor(
+        aliases: string[] = [],
+        description?: string,
+        notes?: string,
+        genre?: string,
+        songs: Song[] = [],
+        socials: string[] = []
+    ) {
+        this.aliases = aliases
+        this.description = description
+        this.notes = notes
+        this.genre = genre
+        this.songs = songs
+        this.socials = socials
     }
-};
+}
 
-/**
- * Contains all the possible availability states.
- */
+
+
+/** Contains all the possible availability states. */
 export const AvailabilityValues = Object.freeze({
-    NONE: new Availability("None", "The artist isn't sure that their music can be used."),
-    VERIFIED: new Availability("Verified", "All music from the artist can be used."),
-    DISALLOWED: new Availability("Disallowed", "All of the artist's music can't be used."),
-    CONTACT_REQUIRED: new Availability("Contact Required", "The artist needs to be contacted."),
-    VARIES: new Availability("Verified", "Some music from the artist can be used.")
+    ALLOWED: "Allowed",
+    DISALLOWED: "Disallowed",
+    VARIES: "Verified",
+    UNKNOWN: "Unknown",
 });
+
+
+
+
+/** Contains information about a specific verified or disallowed song or set of songs. */
+export class Right {
+    identifier: string;
+    isAllowed: boolean;
+
+    constructor(identifier: string, isAllowed: boolean) {
+        this.identifier = identifier;
+        this.isAllowed = isAllowed;
+    };
+};
 
 
 
 export class Artist {
     id: number;
-    name: string;
-    aliases: string[];
-    description: string;
-    notes: string;
-    genre: string;
-    tracks: number;
-    status: states.Status;
-    availability
-    addedAt
-    updatedAt
-    rights
-    socials
 
-    constructor(id, name, aliases, description, notes, genre, tracks, status, availability, addedAt, updatedAt, rights, socials) {
-        this.id = id
-        this.name = name
-        this.aliases = aliases
-        this.description = description
-        this.notes = notes
-        this.genre = genre
-        this.tracks = tracks
-        this.status = status
-        this.availability = availability
-        this.addedAt = addedAt
-        this.updatedAt = updatedAt
-        this.rights = rights
-        this.socials = socials
+    name: string;
+    metadata: ArtistMetadata;
+
+    availability: string;
+    rights: Right[];
+
+    addedAt?: Date;
+    updatedAt?: Date;
+
+    constructor(
+        id: number,
+        name: string,
+        metadata: ArtistMetadata,
+        availability: string,
+        rights: Right[],
+        addedAt?: Date,
+        updatedAt?: Date
+    ) {
+        this.id = id;
+        this.name = name;
+        this.metadata = metadata;
+        this.availability = availability;
+        this.rights = rights;
+        this.addedAt = addedAt;
+        this.updatedAt = updatedAt;
     }
 }
---
+++ src/structs/artistRequest.ts
@@ -0,0 +1,27 @@
+import * as artist from "./artist"
+
+
+/** Contains the artist's status on being contacted */
+ContactStatus {
+    NOT_CONTACTED, // artist hasn't been contacted yet
+    NO_RESPONSE, // artist contacted but no response after some criteria (actually the criteria was already settled over at the pa server) 
+    PENDING_RESPONSE, // artist contacted but response from artist is pending
+    COMPLETED // artist contacted and has responded
+}
+
+
+// artist's status in the approval process
+ApprovalStatus {
+    PENDING, // pending approval by mods
+    COMPLETED // completed approval process
+}
+
+// info about requesting the artist to be added to the database
+ArtistRequest {
+    id: number
+    artist: Artist
+    requestorUserId: number
+    proof: Image[]
+    contactStatus: ContactStatus
+    approvalStatus?: ApprovalStatus // none if ContactStatus is anything other than COMPLETED
+}
